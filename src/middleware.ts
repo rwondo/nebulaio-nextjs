087ba25029af8298c3c5261c90477243b5a7e0b6
th <sryn@tewh.r>
2023-10-30 16:58:13 +0100

    4th commit fixxed auth [i think] hopefully this the last frfr frefr hopefully

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,5 @@
 export { default } from "next-auth/middleware"
 
-export const config = { matcher: ["/dashboard"]}
+export const config = { matcher: ["/dashboard"],
+    secret: process.env.NEXTAUTH_SECRET!
+}
