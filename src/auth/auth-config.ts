fa33cd42f8d892cd58204417b7fd4dbf4f3765fd
th <sryn@tewh.r>
2023-10-30 16:54:12 +0100

    4th commit fixxed auth [i think] hopefully this the last frfr

--- src/auth/auth-config.ts
+++ src/auth/auth-config.ts
@@ -10,6 +10,13 @@ export const authConfig: NextAuthOptions = {
       clientSecret: process.env.WHOP_SECRET!,
     }),
   ],
+  pages: {
+    signIn: "/login",
+    signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+    verifyRequest: '/auth/verify-request', // (used for check email message)
+    newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  },
   callbacks: {
     session({ session, user }) {
       session.user.id = user.id as string;
