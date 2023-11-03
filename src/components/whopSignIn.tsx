7219d96bcc4939705c7422feb0b93ead8f263228
th <sryn@tewh.r>
2023-11-03 17:59:27 +0100

    SPEREM

--- src/components/whopSignIn.tsx
+++ src/components/whopSignIn.tsx
@@ -5,17 +5,14 @@ import { signIn } from "next-auth/react";
 
 import { Button } from "./ui/button";
 import Router from "next/router";
-export function Callback() {
-  const params = new URLSearchParams(window.location.search)
-  const code = params.get('code')
-}
+
 
 
 function WhopSignIn() {
   return (
     <a  href={``}>
     <Button
-      onClick={async () => Callback()}
+      onClick={async () => signIn("whop")}
       className="bg-blue-500 hover:bg-blue-500/80"
     >
       Sign in with Whop 
